3f9a213f3327eadd03fe1b7462ff8219fb084630
sykuang <sykuang.tw@gmail.com>
2025-11-11 14:25:27 +0800

    Refactor VNC server argument handling to streamline configuration and improve readability

--- run.sh
+++ run.sh
@@ -14,17 +14,16 @@ if [ -z "${DISPLAY_HEIGHT}" ]; then
     DISPLAY_HEIGHT=768
 fi
 mkdir -p "${HOME}/.vnc"
-vncserver_args="${DISPLAY}"
+echo "geometry=${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}" >~/.vnc/config
 if [[ -n $VNC_PASSWORD ]]; then
     export PASSWD_PATH="${HOME}/.vnc/passwd"
     echo ${VNC_PASSWORD} | vncpasswd -f >"${PASSWD_PATH}"
-    chmod 0600 "${HOME}/.vnc/passwd"
+    chmod 0600 "${PASSWD_PATH}"
 else
-    vncserver_args=" -SecurityTypes None ${DISPLAY}"
+    echo "SecurityTypes=None" >>~/.vnc/config
 fi
 "${NO_VNC_HOME}"/utils/novnc_proxy --vnc localhost:$((5900+${DISPLAY#:})) --listen ${WEB_PORT} &
-echo "geometry=${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}" >~/.vnc/config
-/usr/libexec/vncserver ${vncserver_args} &
+/usr/libexec/vncserver ${DISPLAY} &
 sleep 2
 pcmanfm --desktop &
 /usr/local/115Browser/115.sh
